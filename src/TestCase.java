a0cdeca5e37eba58e3a080268fde97b303983aa4
Bouton Paul <Paul.Bouton@ensimag.grenoble-inp.fr>
2016-10-11 10:54:34 +0200

    Implémentation de TestCase et modification du makefile

--- src/TestCase.java
+++ src/TestCase.java
@@ -1,8 +1,8 @@
-import classes.*;
+import objects.*;
  
 public class TestCase {
 	public static void main(String args[]) {
-	    Case test = new Case(15, 30);
+	    Case test = new Case(-15, 30, NatureTerrain.FORET);
 	    System.out.println(test); 
     }
 }
---
+++ src/objects/Direction.java
@@ -0,0 +1,7 @@
+package objects;
+public enum Direction {
+        NORD, 
+        SUD, 
+        EST, 
+        OUEST
+}
