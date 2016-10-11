3fe3f7a6236e02b0d3927642ba6bbb3db747b8b2
Bouton Paul <Paul.Bouton@ensimag.grenoble-inp.fr>
2016-10-11 17:45:23 +0200

    implémentation des premières methodes de robot et de celles de drone

--- src/objects/drone.java
+++ src/objects/drone.java
@@ -1,8 +1,11 @@
-
+package objects;
 class Drone extends Robot {
         
 
         public double getVitesse(NatureTerrain NT) {
                 return super.vitesse;
+        }
+        public void deverserEau(int vol){
+        	
         }
 }
