1f138bfa51aa5d522fd741489377fcf2fb011b0d
Tony Buthod <tony.buthod-garcon@ensimag.grenoble-inp.fr>
2016-10-17 14:53:15 +0200

    Création des fichiers Evenement et Simulateur

---
+++ src/animation/Simulateur.java
@@ -0,0 +1,24 @@
+package animation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Simulateur {
+    long dateSimulation;
+
+    public Simulateur() {
+        this.dateSimulation = 0;
+    }
+
+    public void ajouteEvenement(Evenement e) {
+
+    }
+
+    public void incrementeDate() {
+        dateSimulation++;
+    }
+
+    public boolean simulationTerminee() {
+
+    }
+}
